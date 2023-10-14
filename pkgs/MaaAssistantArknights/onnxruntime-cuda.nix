da708f441809af2658571ac2d6131c952573f513
北雁 Cryolitia <Cryolitia@gmail.com>
2023-10-15 00:54:52 +0800

    fix ci

--- pkgs/MaaAssistantArknights/onnxruntime-cuda.nix
+++ pkgs/MaaAssistantArknights/onnxruntime-cuda.nix
@@ -1,4 +1,5 @@
 { stdenv
+, gcc11Stdenv
 , onnxruntime
 , lib
 , fetchpatch
@@ -23,6 +24,7 @@ let
 in (onnxruntime.override {
 
   inherit pythonSupport;
+  stdenv = gcc11Stdenv;
 
 }).overrideAttrs (oldAttrs: rec {
 
