ad567bd1fd986f74a912cb819630973fa81bfc8e
北雁 Cryolitia <Cryolitia@gmail.com>
2023-10-25 11:30:54 +0800

    fix

--- pkgs/common/cuda.nix
+++ pkgs/common/cuda.nix
@@ -10,10 +10,10 @@ rec {
     libcusparse # cusparse.h
     libcufft # cufft.h
     cudnn # cudnn.h
+    cuda_cudart # cuda_runtime.h cuda_runtime_api.h
   ];
 
-  cuda-native-redist = with cudaPackages; [
-      cuda_cudart # cuda_runtime.h cuda_runtime_api.h
+  cuda-native-redist = with cudaPackages; [ 
       cuda_nvcc
     ] ++ cuda-common-redist;
 
