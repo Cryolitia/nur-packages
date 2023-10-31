15d28b69eda7b15b822498195f8cb30aea318f28
北雁 Cryolitia <Cryolitia@gmail.com>
2023-10-31 23:20:18 +0800

    update maa

--- default.nix
+++ default.nix
@@ -30,8 +30,8 @@ rec {
   };
 
   MaaAssistantArknights-beta = MaaAssistantArknights.override {
-    maaVersion = "4.26.0-beta.2";
-    maaSourceHash = "sha256-mWgIrPHtFxeZ1gXtrulcvAFnWS/3rB5M7EgrWfU5riI=";
+    maaVersion = "4.26.0";
+    maaSourceHash = "sha256-1+Z7kGKQ33e7Ma1RpPAv+AYbjl6w6DUxNTtXAviXG2c=";
   };
 
   MaaAssistantArknights-beta-cuda = MaaAssistantArknights-beta.override {
