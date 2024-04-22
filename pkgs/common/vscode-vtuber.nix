e3b06619b323e505111ed371dd764b33aaaa395e
北雁 Cryolitia <Cryolitia@gmail.com>
2024-04-22 14:16:15 +0800

    vscode-vtuber: init

---
+++ pkgs/common/vscode-vtuber.nix
@@ -0,0 +1,19 @@
+{ lib
+, vscode
+, logo-url ? "https://raw.githubusercontent.com/Aikoyori/ProgrammingVTuberLogos/main/VSCode/VSCode.png"
+}:
+
+vscode.overrideAttrs (oldAttrs: rec {
+  postPatch = ''
+    echo ".editor-group-watermark > .letterpress{
+      background-image: url(${logo-url}) !important;
+      opacity: .75;
+    }" >> resources/app/out/vs/workbench/workbench.desktop.main.css
+  '' + oldAttrs.postPatch;
+
+  meta = oldAttrs.meta // {
+    description = "VSCode with github.com/Aikoyori/ProgrammingVTuberLogos LOGO";
+  };
+})
+ 
+ 
