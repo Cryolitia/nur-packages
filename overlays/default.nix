2f86a8a1227ae64f43e96e34eeeb670d901e6f2b
北雁 Cryolitia <Cryolitia@gmail.com>
2024-06-05 11:41:35 +0800

    overlays: update

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,15 @@
+{ packages }:
 {
   # Add your overlays here
-  gnome-text-input-v1 = (prev: final: {
-    gnome = prev.gnome.overrideScope (
-      gfinal: gprev: {
-        mutter = prev.callPackage ../pkgs/by-name/mutter-text-input-v1.nix {};
-        gnome-shell = prev.callPackage ../pkgs/by-name/gnome-shell-fix-preedit-cursor.nix {};
-      }
-    );
-  });
+  nur-cryolitia = (final: prev: { nur-cryolitia = packages."${prev.system}"; });
+  gnome-text-input-v1 = (
+    prev: final: {
+      gnome = prev.gnome.overrideScope (
+        gfinal: gprev: {
+          mutter = gprev.callPackage ../pkgs/pkgs/by-name/mutter-text-input-v1 { };
+          gnome-shell = gprev.callPackage ../pkgs/by-name/gnome-shell-fix-preedit-cursor { };
+        }
+      );
+    }
+  );
 }
