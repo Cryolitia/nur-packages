f01c4110ea4aa2fb82c41274f94acde606d1a130
Cryolitia PukNgae <Cryolitia@gmail.com>
2024-10-22 18:18:06 +0800

    ci: update

--- pkgs/by-name/mdbook-typst-pdf/package.nix
+++ pkgs/by-name/mdbook-typst-pdf/package.nix
@@ -4,26 +4,18 @@
   fetchFromGitHub,
   stdenv,
 }:
-let
-  source = lib.importJSON ./pin.json;
-in
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "mdbook-typst-pdf";
-  version = source.version;
+  version = "0.5.1";
 
   src = fetchFromGitHub {
     owner = "KaiserY";
     repo = "mdbook-typst-pdf";
-    rev = "v${source.version}";
-    hash = source.hash;
+    rev = "v${version}";
+    hash = "sha256-uHsyQOaqRhIITaWhl0AQwzZWousmOX6Cbg74WpzeZ2M=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "typst-0.11.1" = "sha256-FagjVU8BJZStE/geexZERuV2P28iF/pPn2mTi1Gu9iU=";
-    };
-  };
+  cargoHash = "sha256-QhXJZ0F1RTkuPW+bebYr7l9V3V8v/T3kcW1VEcgKTqI=";
 
   meta = with lib; {
     description = "将 mdBook 转换为 PDF。";
